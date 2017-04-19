5c1eb6538fc0409824911b3e2ab39bc82b54ce3d
stowutong <zhangming@sto.cn>
2017-04-19 13:56:10 +0800

    底层框架搭建

---
+++ js/layout/layout.js
@@ -0,0 +1,21 @@
+
+/*	项目名称：梧桐
+ *	文件名称：梧桐系统框架页面Js文件
+ *	开发时间：2017/04/19
+ * 	开发人员：344822559@qq.com
+ * */
+
+
+$(function(){
+	
+	/*窗口内容高度自适应*/
+	$('#main_zm').height($(window).height()-60+'px');
+	$(window).resize(function(){
+		$('#main_zm').height($(window).height()-60);
+	})
+	
+	
+	
+	
+	
+})
